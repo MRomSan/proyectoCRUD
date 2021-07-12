aa9425eeaf37af95cea2f5bc38233a626aeec10c
Manuel <auelrs@hotmail.com>
2021-07-12 18:43:26 +0200

    Aplicación preparada para empezar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AltaComponent } from './alta/alta.component';
+import { ListadoComponent } from './listado/listado.component';
+import { ModificarComponent } from './modificar/modificar.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'listado',
+  component: ListadoComponent
+},
+{
+  path: 'alta',
+  component: AltaComponent
+},
+{
+  path: 'modificar',
+  component: ModificarComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
